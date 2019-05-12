99f65f872c559e92ef12ea6adba210793390abe0
Tharana Mayuranga <tharana.maya@gmail.com>
2019-05-12 14:47:50 +0530

    Added jwt

--- DatingApp.API/Data/AuthRepository.cs
+++ DatingApp.API/Data/AuthRepository.cs
@@ -64,12 +64,11 @@ namespace DatingApp.API.Data
 
         }
 
-        public async Task<bool> UserExits(string username)
+        public async Task<bool> UserExists(string username)
         {
             if (await _context.Users.AnyAsync(x => x.Username == username))
-            {
                 return true;
-            }
+       
             return false;
         }
     }
