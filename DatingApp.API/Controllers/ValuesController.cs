e1983e926724822a5d4e38060f8c1834380e8e97
Tharana Mayuranga <tharana.maya@gmail.com>
2019-05-20 15:44:10 +0530

    Finished Register and login form

--- DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -21,6 +21,7 @@ namespace DatingAPP.API.Controllers
 
         }
         // GET api/values
+        [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> GetValues()
         {
